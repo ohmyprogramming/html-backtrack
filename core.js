6e715686a95030bbf5b034aa840331117cb5800c
ohmyprogramming <126950100+ohmyprogramming@users.noreply.github.com>
2023-03-04 13:59:52 +0100

    Create core.js

---
+++ core.js
@@ -0,0 +1,15 @@
+
+/*
+ * Copyright (C) 2023 ohmyprogramming <github.com/ohmyprogramming>
+ * html-backtrack
+ * JavaScript Function to Backtrack through the DOM tree of an HTML element.
+ */
+
+/**
+ * Backtrack through the DOM tree of an HTML element.
+ * @param HTMLElement element
+ * @param string      nodeName
+ * @param int         maxIterations (-1 is infinite)
+ * @param boolean     getReference (default false) 
+ * @return            Boolean or HTMLElement if getReference is true
+ */
