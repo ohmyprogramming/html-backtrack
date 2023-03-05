19a88b62b3cf2b15f4869b7f0dc338b0223f5f3c
ohmyprogramming <126950100+ohmyprogramming@users.noreply.github.com>
2023-03-05 01:23:30 +0100

    Update core.js

--- core.js
+++ core.js
@@ -9,7 +9,48 @@
  * Backtrack through the DOM tree of an HTML element.
  * @param HTMLElement element
  * @param string      nodeName
+ * @param boolean     getReference (default true)
  * @param int         maxIterations (-1 is infinite)
- * @param boolean     getReference (default false) 
  * @return            Boolean or HTMLElement if getReference is true
  */
+function html_backtrack_element(
+    element       = null,
+    nodeName      = '',
+    getReference  = true,
+    maxIterations = -1
+)
+{
+    if ((element instanceof HTMLElement) === false)
+        return null;
+
+    if (nodeName === '' || nodeName.trim().length === 0)
+        return null;
+
+    nodeName = nodeName.toUpperCase();
+    var i = 0;
+    var ref = null;
+
+    while (element != null)
+    {
+        if (element.nodeName === nodeName)
+        {
+            ref = element;
+            break;
+        }
+        element = element.parentElement;
+    }
+
+    if (getReference == true)
+    {
+        return ref;
+    }
+
+    if (ref === null)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
